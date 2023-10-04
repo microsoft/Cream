0f43dc2dd2aa5a88364c242abb2ef99bf84c9d94
root <root@21bb73ff3d1a4d9f8e24df1356ba4b040000CC.3raleiv44c3udcfl4riolcspoc.jx.internal.cloudapp.net>
2023-10-04 13:25:53 +0000

    script

---
+++ TinyCLIP/script/manual_weight_inherit_75to50.sh
@@ -0,0 +1,28 @@
+export NNODES=1
+export GPUS_PER_NODE=8
+
+DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES"
+torchrun $DISTRIBUTED_ARGS src/training/main.py \
+ --save-frequency 1 \
+ --report-to wandb \
+ --train-data synthetic \
+ --dataset-type synthetic \
+ --imagenet-val ./ImageNet \
+ --warmup 2000 \
+ --batch-size 1024 \
+ --epochs 6 \
+ --workers 8 \
+ --model TinyCLIP-40M \
+ --name exp_name \
+ --seed 0 \
+ --local-loss \
+ --grad-checkpointing \
+ --logs ./outputs/ViT-B-32 \
+ --lr 0.0001 \
+ --gather-with-grad \
+ --pretrained-image-file checkpoints/TinyCLIP-ViT-61M-32_epoch_6.pt \
+ --pretrained-text-file checkpoints/TinyCLIP-ViT-61M-32_epoch_6.pt \
+ --distillation-teacher ViT-B-32@laion2b_e16 \
+ --logit-scale 50 \
+ --norm_gradient_clip 5 \
+ --train-num-samples 400000000 \
